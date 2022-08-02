f200e357db6ae1d29a43d35445b8517fdc07423d
jinx420 <majoad.0.tv@gmail.com>
2022-08-02 22:58:54 +0200

    Fixed issues

--- Client.py
+++ Client.py
@@ -4,7 +4,6 @@ import socket
 import string
 import subprocess
 import sys
-<<<<<<< HEAD
 import random
 
 from threading import Thread
@@ -14,12 +13,6 @@ from datetime import datetime
 # THIS IS A DEBUG VERSION USE CLIENT.PYW INSTEAD 
 
 HOST = sys.argv[1] if len(sys.argv) > 1 else '192.168.2.198' # Change this to your public ip if you want to use it outside of your local network
-=======
-
-# THIS IS A DEBUG VERSION USE CLIENT.PYW INSTEAD 
-
-HOST = sys.argv[1] if len(sys.argv) > 1 else 'IP ADDRESS OF THE SERVER' # Change this to your public ip if you want to use it outside of your local network
->>>>>>> origin
 PORT = int(sys.argv[2] if len(sys.argv) > 2 else 5555) # Change the port if you want to but make sure to change it in Client.py as well
 print('Please wait while we establish a connection...')
 s = socket.socket()
